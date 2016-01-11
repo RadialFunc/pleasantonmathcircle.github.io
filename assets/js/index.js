18fd1744442f263a67850adba172a7834ac9de5e
akshayravikumar <akshayr@mit.edu>
2016-01-10 18:10:38 -0800

    smooth scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+	smoothScroll.init({
+		speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+    	easing: 'easeInOutCubic', // Easing pattern to use
+	});
+
 	adjust = function() {
 	  	if ($(window).scrollTop() <= 0) {
 	        $('#navbar').removeClass('opaque');
